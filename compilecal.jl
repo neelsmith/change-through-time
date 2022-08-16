0c52b1aad4516c81ecbdac84fe2d8af83b9f87e6
Neel Smith <dnsmith.neel@gmail.com>
2022-08-16 18:01:37 -0400

    Work on initial schedule

--- compilecal.jl
+++ compilecal.jl
@@ -3,8 +3,8 @@ Pkg.activate(".")
 Pkg.instantiate()
 using CourseCalendar
 
-calfile = joinpath(pwd(), "F22", "calendar1.toml")
-topics = joinpath(pwd(), "F22", "topics1.txt")
+calfile = joinpath(pwd(), "S23", "calendar1.toml")
+topics = joinpath(pwd(), "S23", "topics1.txt")
 sched = courseSchedule(calfile, topics)
 
 md = mdcalendar(sched)
